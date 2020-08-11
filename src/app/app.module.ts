6e18736173cfed963a72643362eb6685d4c62c55
william.diniz <william.diniz@tapps.com.br>
2020-08-11 03:43:49 -0300

    Adding Nav Menu and Welcome Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
 import {ProjectListComponent} from './projects/project-list.component';
-import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     ProjectListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'projects', component: ProjectListComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: '**', redirectTo: 'home', pathMatch: 'full'}
+    ]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
